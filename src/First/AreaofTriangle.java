73220077adae143a2f6b4af910a3828635aea935
Md. Tanvir Sadik Raj <32238769+TSraj@users.noreply.github.com>
2022-06-07 00:19:44 +0600

    areaoftraingle

---
+++ src/First/AreaofTriangle.java
@@ -0,0 +1,19 @@
+package First;
+
+import java.util.Scanner;
+
+public class AreaofTriangle {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		double base, height, area;
+		
+		System.out.println("Enter base: ");
+		base = input.nextDouble();
+		System.out.println("Enter height: ");
+		height = input.nextDouble();
+		
+		area = 0.5 * base * height;
+		System.out.println("The area of triangle is: " +area);
+	}
+}
