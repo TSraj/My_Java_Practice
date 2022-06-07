e4c7e94f9680baecd85c7ea2a04f24eb37475afb
Md. Tanvir Sadik Raj <32238769+TSraj@users.noreply.github.com>
2022-06-07 22:09:25 +0600

    problem_01

--- src/bookChapterOneProblems/problem_001.java
+++ src/bookChapterOneProblems/problem_001.java
@@ -1,7 +1,25 @@
 package bookChapterOneProblems;
 
+import java.util.Scanner;
+
 public class problem_001 {
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int num1, num2, sum, sub, mul, div;
 		
+		System.out.println("Enter number one: ");
+		num1 = input.nextInt();
+		System.out.println("Enter number two: ");
+		num2 = input.nextInt();
+		
+		sum = num1+num2;
+		sub = num1-num2;
+		mul = num1*num2;
+		div = num1/num2;
+		
+		System.out.println("The sum is: "+sum);
+		System.out.println("The sub is: "+sub);
+		System.out.println("The mul is: "+mul);
+		System.out.println("The div is: "+div);
 	}
 }
