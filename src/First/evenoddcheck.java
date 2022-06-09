6d6b33fb00096dbe5094669469e40b4a2bcc6b47
Md. Tanvir Sadik Raj <32238769+TSraj@users.noreply.github.com>
2022-06-10 00:46:05 +0600

    evenoddcheck

---
+++ src/First/evenoddcheck.java
@@ -0,0 +1,19 @@
+package First;
+
+import java.util.Scanner;
+
+public class evenoddcheck {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int num;
+		
+		System.out.println("Enter a number: ");
+		num = input.nextInt();
+		
+		if (num%2==0) {
+			System.out.println("Even");
+		}else {
+			System.out.println("Odd");
+		}
+	}
+}
