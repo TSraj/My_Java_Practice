8ce5119625ed773e19aeea98a83d2cee3fc4fc09
Md. Tanvir Sadik Raj <32238769+TSraj@users.noreply.github.com>
2022-06-11 01:27:50 +0600

    typeconversionfrom_chartoint

---
+++ src/bookChapterOneProblems/type_Coversionfromchartoint.java
@@ -0,0 +1,9 @@
+package bookChapterOneProblems;
+
+public class type_Coversionfromchartoint {
+	public static void main(String[] args) {
+		char a = 'a';
+		int asciiValue = a;
+		System.out.println("Ascii Value of a is: " +asciiValue);
+	}
+}
