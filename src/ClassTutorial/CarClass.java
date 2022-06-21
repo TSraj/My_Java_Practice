e28fe82b13d59e7191d29ad8560a07f0c8aeab6f
Md. Tanvir Sadik Raj <32238769+TSraj@users.noreply.github.com>
2022-06-21 17:04:22 +0600

    work on basic practice

--- src/ClassTutorial/CarClass.java
+++ src/ClassTutorial/CarClass.java
@@ -2,26 +2,36 @@ package ClassTutorial;
 
 public class CarClass {
 	public String manufacturerName;
-	public String modelName;
-	public int engineCC;
-	public float carWeight;
-	public float fuelAmount;
+	private String modelName;
+	
+	private String carKeyValue;
+	
+	int engineCC; // default
+	private float fuelAmount;
+	final private float amountOfFuelConsumedPerSeconds = 0.02f;
 	
 	public CarClass() {
 		// TODO Auto-generated constructor stub
 		manufacturerName = new String();
 		modelName = new String();
-		engineCC = 0;
-		carWeight = 0;
+		engineCC = 1000;
 		fuelAmount = 0;
 	}
 	
 	public CarClass(String carName) {
 		manufacturerName = new String(carName);
 		modelName = new String();
-		engineCC = 0;
-		carWeight = 0;
+		engineCC = 0 ;
 		fuelAmount = 0;
+	}
+	
+	//constructor
+	public CarClass(String manu, String model, String key, int engineCC, float fuel) {
+		manufacturerName = manu;
+		modelName = model;
+		carKeyValue = key;
+		this.engineCC = engineCC;
+		fuelAmount = fuel;
 	}
 	
 	public float presentAmountOffuel() {
@@ -29,5 +39,21 @@ public class CarClass {
 	}
 	public String getManufacturerName() {
 		return manufacturerName;
+	}
+	public String getModelName() {
+		return modelName;
+	}
+	
+	public void addFuel(float fuelAmount) {
+		this.fuelAmount+= fuelAmount;
+	}
+	
+	public void runForSeconds(int timeInSecond) {
+		float usedFuel = timeInSecond*amountOfFuelConsumedPerSeconds;
+		fuelAmount-=usedFuel;
+	}
+	
+	public boolean matchPassword(String userInput) {
+		return userInput.equals(carKeyValue);
 	}
 }
--- src/ClassTutorial/learningClass.java
+++ src/ClassTutorial/learningClass.java
@@ -1,4 +1,4 @@
-package ClassTutorial;
+package ClassTutorial;import First.UserInput1;
 
 public class learningClass {
 	
@@ -7,8 +7,17 @@ public class learningClass {
 	static private CarClass ourCar=new CarClass();
 	static private CarClass ourAnotherCar; 
 	public static void main(String[] args) {
-		ourCar.presentAmountOffuel();
-		String name = ourCar.manufacturerName;
+		ourAnotherCar = new CarClass("Toyota", "Alion", "qwert", 1500, 30);
+		System.out.println("Manufacturer: "+ourAnotherCar.manufacturerName);
+		System.out.println("Model Name: "+ourAnotherCar.getModelName());
+		System.out.println("Password Match: "+ourAnotherCar.matchPassword("qwertyuiop"));
+		System.out.println("2nd car engine CC:"+ourAnotherCar.engineCC);
 		
+		System.out.println("amount of fuel: "+ourAnotherCar.presentAmountOffuel()+"L ");
+		ourAnotherCar.addFuel(5);
+//		System.out.println("We have added: "+ourAnotherCar.addFuel(5)+"Liter fuel");
+		System.out.println("amount of fuel: "+ourAnotherCar.presentAmountOffuel()+"L ");
+		ourAnotherCar.runForSeconds(60);
+		System.out.println("amount of fuel: "+ourAnotherCar.presentAmountOffuel()+"L ");
 	}
 }
